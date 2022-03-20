ebcb85469e2b52f3d300768296d327b966575a0b
omidprogrammer <omid.sh.2003@gmail.com>
2022-03-20 17:31:07 +0330

    useReducer api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import React, { Component, Suspense } from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import React from "react";
+import {
+  useStateDispatch,
+  useDispatchState,
+} from "./components/store/Provider";
 
-import HomePage from "./Components/HomePage";
-import Blogs from "./Components/Blogs";
-import AboutUS from "./Components/AboutUS";
-import NotFound from "./Components/Error404";
-
-import TelMe from "./Components/AboutUs-nesting/telMe";
-import Roules from "./Components/AboutUs-nesting/Rouls";
-
-const Navbar = React.lazy(() => import("./Components/Navbar"));
-
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Suspense fallback={<h4>loading....</h4>}>
-          <Navbar />
-        </Suspense>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/aboutus" element={<AboutUS />}>
-            <Route path=":telme" element={<TelMe />} />
-            <Route path="roules" element={<Roules />} />
-          </Route>
-          <Route path="/blogs/*" element={<Blogs />} />
-          <Route path="/notFound" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/notFound" />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  const dispatch = useStateDispatch();
+  const { data, age } = useDispatchState();
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: "SET_DATA", payload: {name: "horaaaaaaa"} })}>
+        set name
+      </button>
+      <button onClick={() => dispatch({ type: "SET_AGE", payload: 19 })}>
+        set age
+      </button>
+      <h1>{data.name}</h1>
+      <h1>{age}</h1>
+    </div>
+  );
 }
--- src/Components/Error404.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Error404 extends Component {
-  render() {
-    return <h1 style={{textAlign: "center"}}>notFound</h1>;
-  }
-}
