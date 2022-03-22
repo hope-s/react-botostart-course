3cc0825254a8e675866ef8ca2381f4c10e93c578
omidprogrammer <omid.sh.2003@gmail.com>
2022-03-22 19:23:09 +0430

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
-import React from "react";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SingIn";
-import { ToastContainer } from "react-toastify";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div>
-      <ToastContainer />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/" element={<Navigate to="/signIn" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      items: [],
+    };
+    this.handelChange = this.handelChange.bind(this);
+    this.handelSubmit = this.handelSubmit.bind(this);
+  }
+
+  handelSubmit(e){
+    e.preventDefault();
+    if(this.state.text.length === 0){
+      return 
+    }
+
+    const newItems = {
+      text: this.state.text,
+      id: Date.now()
+    }
+
+    this.setState((prev)=>({
+      items: prev.items.concat(newItems),
+      text: ""
+    }))
+    console.log(this.state)
+  }
+  
+  handelChange(event) {
+    this.setState({ text: event.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handelSubmit}>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handelChange}
+          />
+          <button>Add</button>
+        </form>
+        <ul>
+          {this.state.items.map((item, id) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
