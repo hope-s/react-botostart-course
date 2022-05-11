42800e23eb2b698a17a09ba91766d8c9f5e85d91
omidprogrammer <omid.sh.2003@gmail.com>
2022-05-11 17:41:31 +0430

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import Demo from "./components/Demo";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { useMemo, useState } from "react";
 
-function App() {
+export default function App() {
+  const [show, setShow] = useState(true);
+  const [number, setNumber] = useState(1);
+
+  const slow = useMemo(() => {
+    for (let index = 0; index < 1000000000; index++) {}
+    return show;
+  }, [show]);
+
+  function setShowFunc() {
+    setShow(!show);
+  }
+
+  function setFuncNumber() {
+    setNumber(number + 1);
+  }
+
   return (
-    <Provider store={store}>
-      <Demo />
-    </Provider>
+    <div className="App">
+      <h1>{slow ? "true" : "false"}</h1>
+      <p>{number}</p>
+      <button onClick={setFuncNumber}>set number</button>
+      <button onClick={setShowFunc}>set show</button>
+    </div>
   );
 }
-
-export default App;
